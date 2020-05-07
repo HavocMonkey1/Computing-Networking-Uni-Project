449292c0d725a52e54ebb080cf3b066374b28878
HavocMonkey1 <matthew@monaghan.co.uk>
2020-05-07 21:13:59 +0100

    Talking to dummy server

--- Client GUI/WindowsFormsApp1/Login Window.cs
+++ Client GUI/WindowsFormsApp1/Login Window.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -22,7 +24,25 @@ namespace WindowsFormsApp1
 
         private void LoginButton_Click(object sender, EventArgs e)
         {
-            throw new System.NotImplementedException();
+            //temp request demo 
+            {
+                using (var client =  new TcpClient( hostname: "127.0.0.1", port: 80))
+                {
+                    using (var stream = client.GetStream())
+                    using(var writer = new StreamWriter(stream))
+                    using (var reader = new StreamReader(stream))
+                    {
+                        writer.AutoFlush = true;
+                        writer.WriteLine("GET / HTTP/1.1");/*Get is the verb, / is the root directory, HTTP/1.1 is the protocol*/
+                        writer.WriteLine("Host: 127.0.0.1:80");/*the address of the server*/
+                        //Double blank line signifies end of header in a HTTP request
+                        writer.WriteLine();
+                        writer.WriteLine();
+
+                        MessageBox.Show(reader.ReadToEnd());/*This create a message box and dumps the output from the variable reader*/
+                    }
+                }
+            }
         }
         
     }
